ee0479f4c4362c09d5ec1808a09f336edf004205
Rich Zaraspe <zglennrichell06@gmail.com>
2025-04-11 13:53:24 +0800

    Fixed minor merge issues from previous commit

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -6,6 +6,7 @@ import Homepage from "./Pages/homepage";
 import UploadPage from "./Pages/upload";
 import Display from "./Pages/display";
 import EmailForm from "./Components/EmailForm"; // Import the new component
+import FormulaPage from "./Pages/formulaPage";
 
 function App() {
     return (
@@ -15,6 +16,7 @@ function App() {
                 <Route path="/homepage" element={<Homepage />} />
                 <Route path="/Upload-Page" element={<UploadPage />} />
                 <Route path="/Display-Page" element={<Display />} />
+                <Route path="/Formula-Page" element={<FormulaPage />} />
                 <Route path="/email" element={<EmailForm />} /> {/* New route for email form */}
             </Routes>
         </Router>
